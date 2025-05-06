8f9396cd99ba8d4f8c909d6f4cc1146275af5bba
ValHDL <valerie.houdaille@hinodev.com>
2025-05-06 19:35:04 +0200

    Test creation ressource

--- app/Http/Controllers/API/RelationTypeController.php
+++ app/Http/Controllers/API/RelationTypeController.php
@@ -83,7 +83,7 @@ class RelationTypeController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified resource from storage. 
      */
     public function destroy($id)
     {
